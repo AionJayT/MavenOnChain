a93854a71a48e6b857037c697a1e7c7464e7d036
Jeff Disher <jeffdisher@nuco.io>
2019-11-26 14:05:31 -0500

    Fleshed out command-line options

--- Reader/src/Reader.java
+++ Reader/src/Reader.java
@@ -1,4 +1,5 @@
 import org.aion.maven.blockchain.BlockchainReader;
+import org.aion.maven.blockchain.Codecs;
 import org.aion.maven.blockchain.Rpc;
 import org.aion.maven.state.ProjectedState;
 import org.aion.maven.types.Address;
@@ -10,17 +11,40 @@ import io.ipfs.multihash.Multihash;
 
 public class Reader {
     public static void main(String[] args) throws Throwable {
-        // For now, the port is just hard-coded.
-        int port = 2000;
-        String kernelHostname = "localhost";
-        int kernelPort = 8545;
+        String kernelHostname = null;
+        int kernelPort = -1;
+        String ipfsHostname = null;
+        int ipfsPort = -1;
+        int port = -1;
         Address contractAddress = null;
-        long startingBlockNumber = 1;
+        long startingBlockNumber = -1L;
+        try {
+            kernelHostname = parseOption(args, "--kernelHostname");
+            kernelHostname.toString();
+            kernelPort = Integer.parseInt(parseOption(args, "--kernelPort"));
+            ipfsHostname = parseOption(args, "--ipfsHostname");
+            ipfsHostname.toString();
+            ipfsPort = Integer.parseInt(parseOption(args, "--ipfsPort"));
+            port = Integer.parseInt(parseOption(args, "--listenPort"));
+            contractAddress = new Address(Codecs.hexStringToBytes(parseOption(args, "--contractAddress")));
+            startingBlockNumber = Long.parseLong(parseOption(args, "--startingBlockNumber"));
+        } catch (Exception e) {
+            System.err.println("Usage:  Reader <options>"
+                    + "\n\t--kernelHostname kernelHostname - The hostname/IP of the Aion kernel"
+                    + "\n\t--kernelPort kernelPort - The RPC port of the Aion kernel"
+                    + "\n\t--ipfsHostname ipfsHostname - The hostname/IP of the IPFS node"
+                    + "\n\t--ipfsPort ipfsPort - The port of the IPFS node"
+                    + "\n\t--listenPort listenPort - The port this web server will use"
+                    + "\n\t--contractAddress contractAddress - The address of the contract to observe"
+                    + "\n\t--startingBlockNumber startingBlockNumber - The block where the read should start"
+            );
+            System.exit(1);
+        }
         
         // Create all the components.
         ProjectedState<Multihash> projection = new ProjectedState<>();
         Rpc rpc = new Rpc(kernelHostname, kernelPort);
-        IPFS ipfs = new IPFS("localhost", 5001);
+        IPFS ipfs = new IPFS(ipfsHostname, ipfsPort);
         BlockchainReader reader = new BlockchainReader(ipfs, projection, rpc, contractAddress, startingBlockNumber);
         WebServer server = new WebServer(ipfs, projection);
         
@@ -45,5 +69,16 @@ public class Reader {
         System.out.println("Shutting down reader...");
         reader.stop();
         server.stop();
+    }
+
+
+    private static String parseOption(String[] args, String option) {
+        String result = null;
+        for (int i = 0; i < args.length; ++i) {
+            if (option.equals(args[i]) && ((i + 1) < args.length)) {
+                result = args[i+1];
+            }
+        }
+        return result;
     }
 }
